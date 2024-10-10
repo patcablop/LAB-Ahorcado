502ac8bc9bf5d8c023bbd9882d006cb596c42941
patcablop <pcaballero006@gmail.com>
2024-10-10 18:50:48 +0200

    Ejercicio 1 apartado c

--- src/ahorcado.py
+++ src/ahorcado.py
@@ -1,3 +1,5 @@
+
+import random
 def cargar_palabras(ruta):
     '''
     Recibe la ruta de un fichero de texto que contiene una palabra por línea y devuelve
@@ -8,3 +10,15 @@ def cargar_palabras(ruta):
         for linea in f:
             res.append(linea.strip()) # strip() elimina los espacios en blanco y saltos de línea al principio y al final
         return res
+    
+def elegir_palabra(palabras):
+    '''
+    Elige la palabra a adivinar:
+    - Selecciona una palabra aleatoria de la lista 'palabras'
+    - Devuelve la palabra seleccionada
+    Ayuda: 
+    - La función 'random.choice' del paquete 'random' recibe una lista de opciones y 
+      devuelve una de ellas seleccionada aleatoriamente.
+    '''
+    palabra_aleatoria = random.choice(palabras)
+    return palabra_aleatoria
